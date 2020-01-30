6096ffe6ee55676819900f69b3e63686f9384a41
RomainLanglois <romain.langlois@viacesi.fr>
2020-01-30 20:39:45 +0100

    Update one_way.sh script

--- scripts/one_way.sh
+++ scripts/one_way.sh
@@ -16,11 +16,10 @@ ld -m elf_i386 -z execstack -o $1 $1.o
 echo "[+] Done !"
 
 echo ""
-
-## TODO: REPLACE the file creation by an awk way  
+ 
 # Generating shellcode from the nasm file
 echo "[!] Getting shellcode..."
-objdump -d ./decoder |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g' > /tmp/result.txt
+objdump -d ./$1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g' > /tmp/result.txt
 shellcode=`cat /tmp/result.txt`
 rm /tmp/result.txt
 echo "[+] Done !"
